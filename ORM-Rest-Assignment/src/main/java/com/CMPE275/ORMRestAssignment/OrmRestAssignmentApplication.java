941851e52e2cd5d2e4d4809a5ec5e1bb5c38a887
MeeraTresa <meeratresa@gmail.com>
2023-04-15 15:46:39 -0700

    Initial commit

---
+++ ORM-Rest-Assignment/src/main/java/com/CMPE275/ORMRestAssignment/OrmRestAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.CMPE275.ORMRestAssignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrmRestAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrmRestAssignmentApplication.class, args);
+	}
+
+}
